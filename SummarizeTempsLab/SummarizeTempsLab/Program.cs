5a27bedca3d3a1ba1770d170e56df8585412b5cd
dschnoor2 <59981825+dschnoor2@users.noreply.github.com>
2020-02-15 12:18:33 -0600

    Update Program.cs
    Stream Writer Added, output file Added and Code cleaned up.

--- SummarizeTempsLab/SummarizeTempsLab/Program.cs
+++ SummarizeTempsLab/SummarizeTempsLab/Program.cs
@@ -9,43 +9,41 @@ namespace SummarizeTempsLab
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("***Summarize Temperatures Lab***");
             string fileName;
              
             bool userContinue = true;
             string choice = "";
 
-
             // temperature data is in temps.txt
             // Write out prompt to the console
             Console.WriteLine("Enter filename");
-            // Read the filename from the console
             fileName = Console.ReadLine();
 
             // Test whether file exists
 
             // If the file exists
             if (File.Exists(fileName))
-            {
-                Console.WriteLine("File Exist");
+                {
+              Console.WriteLine("File Exist");
                            
                 //Loop for userContinue
                 while (userContinue)
                 {
-                    // Ask the user to enter the temperature threshold
-                    Console.WriteLine("Enter Threshold");
+                  // Ask the user to enter the temperature threshold
+                  Console.WriteLine("Enter Threshold");
 
-                    string input;
-                    int threshold;
-                    input = Console.ReadLine();
-                    threshold = int.Parse(input);
+                  string input;
+                  int threshold;
+                  input = Console.ReadLine();
+                  threshold = int.Parse(input);
 
-                    int sumTemps = 0;
-                    int numAbove = 0;
-                    int numBelow = 0;
+                  int sumTemps = 0;
+                  int numAbove = 0;
+                  int numBelow = 0;
 
-                    // Open the file and create Steam Reader
-                    // Read a line into a string variable
+                  // Open the file and create Steam Reader
+                  // Read a line into a string variable
                     using (StreamReader sr = File.OpenText(fileName))
                     {
                         string line = sr.ReadLine();
@@ -57,70 +55,73 @@ namespace SummarizeTempsLab
                             // Convert (parse) into an integer variable
                             temp = int.Parse(line);
 
-                            // Add Temperature to summing variable
-                            sumTemps += temp;
-                            
-
-                            // If the current temperature value >= threshold 
-                            if (temp >= threshold)
-                            {
-                                // Increment "above" counter by 1
-                                numAbove += 1;
-                            }
-                            // Else (temperature is below)
-                            else
-                            {
+                           // Add Temperature to summing variable
+                           sumTemps += temp;
+                          
+                           // If the current temperature value >= threshold 
+                           if (temp >= threshold)
+                           {
+                              // Increment "above" counter by 1
+                              numAbove += 1;
+                           }
+                              // Else (temperature is below)
+                           else
+                           {
                                 
-                                // Increment "below" counter by 1
-                                numBelow += 1;
-                            }
+                               // Increment "below" counter by 1
+                               numBelow += 1;
+                           }
 
-                            line = sr.ReadLine();
+                                line = sr.ReadLine();
                         }
                     }
 
                     // Print out temperatures above the threshold
-                    Console.WriteLine("Temps Above = " + numAbove);
+                    Console.WriteLine("Temperatures Above = " + numAbove);
+                    Console.WriteLine("Temperatured Below = " + numBelow);
 
-                    // Print out temperatures below the threshold
-                    Console.WriteLine("Temps below = " + numBelow);
-
-                    // Calculate the average
                     int average = sumTemps / (numAbove + numBelow);
 
-                    // Print out average
                     Console.WriteLine("Average Temperature = " + average);
 
                     //Print out Total Temps
                     Console.WriteLine("Total number of Temperatures = " + (numAbove +numBelow));
+
+                    using (StreamWriter sw = new StreamWriter("output.txt"))
+                    {
+                        sw.WriteLine("***Summary Temperatures Lab***");
+                        sw.WriteLine(System.DateTime.Now.ToString());
+                        sw.WriteLine("Temps Above = " + numAbove);
+                        sw.WriteLine("Temps Below = " + numBelow);
+                        sw.WriteLine("Average Temp = " + average);
+                        sw.WriteLine("Total Number of Temperatures = " + (numAbove + numBelow));
+                    }
                 
                     //Write Line to console "Do you Wish to Continue?
-                     Console.WriteLine("Do you wish to coninue? Enter yes or no");
-                     choice = Console.ReadLine();
+                    Console.WriteLine("Do you wish to coninue? Enter yes or no");
+                    choice = Console.ReadLine();
 
-                
                     if (choice == "yes")
                     {
-                        continue;
+                       continue;
                     }
-
                     else
                     {
-                        userContinue = false;
+                       userContinue = false;
 
-                        //Write Line to Console "Program has Ended"
-                        Console.WriteLine("Program has Ended...THANK YOU!");
+                       //Write Line to Console "Program has Ended"
+                       Console.WriteLine("Program has Ended...THANK YOU!");
                     }
                
                 }
             }
-            else
-            {
-                //Else (file does not exist)
-                //Tell the user the file does not exist
-                Console.WriteLine("File does not exist");
-            }
-
+                else
+                {
+                    //Else (file does not exist)
+                    //Tell the user the file does not exist
+                    Console.WriteLine("File does not exist!");
+                }
+            
         }
 
     }
